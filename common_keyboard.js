135540fcf9fec2f4cd84de053fbde966c4ddc7fb
math-miki <math.micky.dtl@gmail.com>
2018-10-04 07:03:15 +0900

    added ls,cd,cat. can cd miki.bio

--- common_keyboard.js
+++ common_keyboard.js
@@ -9,7 +9,7 @@ window.onload = function() {
   // 制御関数
   function keydown(e) {
     if(e.key === 'Enter') {
-
+      analysisCommand();
     } else if(e.key === 'Backspace') {
       tmpSentense = tmpSentense.slice(0,-1);
       updateConsoleContext();
@@ -23,7 +23,53 @@ window.onload = function() {
   function updateConsoleContext() {
     consoleEl.innerHTML = initsentense+nowPath+' $ '+tmpSentense+'<span id="cursor"></span>'
   }
+  function analysisCommand() {
+    // emmit first spaces
+    var commands = tmpSentense.split(" ").filter(element=> element!=='');
+    // reaching here means something which can be commands typed
+    // commands[0] means typed commands
+    // commands[1:] means options of commands
+    switch(commands[0]) {
+      case 'ls':
+        /* TODO:ls */
+        break;
+      case 'cd':
+        /* TODO:cd */
+        cd(commands.slice(1));
+        break;
+      case 'cat':
+        /* TODO:cat */
+        break;
+      default:
+        // command didn't make any sense
+        /* TODO:byebye */
+        break;
+    }
+
+
+  }
   function generateNewSentense(past, nextPath) {
     return past + "<br>" + nextPath + '<span id="cursor"></span>'
+  }
+  /* command functons
+  - ls
+  - cd
+  - cat
+  */
+  function ls(optionArr) {
+    const options = optionArr.filter(option => option!=='');
+
+  }
+  function cd(optionArr) {
+    const options = optionArr.filter(option => option!=='');
+    // 'cd miki.bio': jump to miki.bio
+    if (options.length===0) {/* TODO:空の処理 */}
+    if (options[0]=='miki.bio') {
+      window.location.href = 'http://miki.bio';
+    }
+  }
+  function cat(optionArr) {
+    const options = optionArr.filter(option => option!=='');
+
   }
 }
